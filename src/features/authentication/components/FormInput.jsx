34f52fa0565934a9e36889eff978ca2195176eb7
Matthew Tran <tran28@my.yorku.ca>
2024-03-03 04:04:07 -0500

    FIX: form sizing on mobile view

--- src/features/authentication/components/FormInput.jsx
+++ src/features/authentication/components/FormInput.jsx
@@ -6,7 +6,7 @@ function FormInput({ label, type, name, placeholder, value, onChange, error }) {
                 type={type}
                 id={name}
                 name={name}
-                className="text-sm p-3 bg-accent-aman border-2 border-accent-gray focus:border-accent-dark focus:outline-none"
+                className="text-base p-3 bg-accent-aman border-2 border-accent-gray focus:border-accent-dark focus:outline-none sm:text-sm"
                 placeholder={placeholder}
                 value={value}
                 onChange={onChange}
