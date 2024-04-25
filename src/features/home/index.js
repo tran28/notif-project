838773df769d33e6e1159a1fa7e9fc926d097344
Matthew Tran <tran28@my.yorku.ca>
2024-04-24 23:54:34 -0400

    UPDATE: homepage

--- src/features/home/index.js
+++ src/features/home/index.js
@@ -28,29 +28,28 @@ function Home() {
 
     return (
         <div className="flex flex-col gap-10">
-            <div className="grid grid-cols-10 h-1/2 items-center overflow-hidden relative">
+            <div className="grid grid-cols-10 h-auto items-center overflow-hidden relative md:h-[65vw]">
                 <motion.div
-                    className="col-start-1 col-span-10 w-full h-auto flex items-start justify-start self-start text-white z-30 px-6 py-4 lg:px-12 lg:py-8 md:col-start-1 md:col-span-3"
+                    className="col-start-1 col-span-10 w-full h-auto flex items-start justify-start self-start text-white z-30 px-0 py-4 lg:px-12 lg:py-8 md:col-start-1 md:col-span-3 md:px-6"
                 >
-                    <div className="flex flex-col text-black text-base gap-1 md:text-lg" >
-                        <p className="cursor-pointer hover:text-accent-mid" onClick={() => handleClick('faq')}>&gt; FAQ</p>
-                        <p className="cursor-pointer hover:text-accent-mid" onClick={() => handleClick('dev')}>&gt; DEV Corner</p>
+                    <div className="flex flex-col gap-1 items-start" >
+                        <Button className="bg-accent-mid text-accent-dark text-sm md:text-lg " bgHover={colors.black} textHover={colors.accent.light} onClick={() => handleClick('faq')}>&gt; FAQ</Button>
+                        <Button className="bg-accent-mid text-accent-dark text-sm md:text-lg " bgHover={colors.black} textHover={colors.accent.light} onClick={() => handleClick('dev')}>&gt; DEV Corner</Button>
                     </div>
                 </motion.div>
                 <motion.div
-                    className="bg-accent-mid col-start-1 col-span-10 w-full h-full flex items-center justify-center text-white z-10 md:col-start-4 md:col-span-7"
+                    className="bg-accent-mid col-start-1 col-span-10 w-full h-full flex items-center justify-center text-white z-10 md:col-start-4 md:col-span-7 md:flex"
                 >
                     <div className="relative w-full h-full"> {/* Container for image and overlay */}
-                        <img src="https://notif-assets.s3.amazonaws.com/shopping-cart.webp" className="object-cover h-full w-full saturate-100" />
+                        <img src="https://notif-assets.s3.amazonaws.com/shopping-cart.webp" className="object-cover h-full w-full saturate-100 object-left" />
                         <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-accent-contrast via-transparent to-transparent md:bg-gradient-to-r"></div> {/* Overlay */}
-                        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-accent-contrast via-transparent to-transparent md:bg-gradient-to-l"></div> Overlay
                     </div>
                 </motion.div>
                 <motion.div
                     style={{ y: y1 }}
-                    className="bg-accent-mid col-start-3 col-span-7 w-full flex flex-col items-start relative z-20 p-8 gap-4 transform !-translate-y-[40%] lg:p-16 md:col-start-2 md:col-span-5 md:absolute md:!translate-y-0"
+                    className="bg-accent-mid col-start-1 col-span-10 w-full flex flex-col items-start relative z-20 p-8 gap-4 lg:p-16 md:col-start-2 md:col-span-5 md:absolute"
                 >
-                    <p className="text-accent-dark text-4xl font-extrabold md:text-[56px] lg:text-[72px]">no•tif</p>
+                    <p className="text-accent-dark text-4xl font-extrabold md:text-[48px] lg:text-[64px] lg:pb-2">no•tif</p>
                     <div className="flex flex-col gap-1 text-black text-xs md:text-sm lg:text-base">
                         <p>Missed the last <span className="text-accent-dark italic font-bold">price drop</span>?</p>
                         <p>Be <span className="text-accent-dark italic font-bold">notified</span> next time!</p>
