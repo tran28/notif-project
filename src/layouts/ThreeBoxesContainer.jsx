664e61b1f2738dad2a4c27e69bd999f5d8bec2a8
Matthew Tran <tran28@my.yorku.ca>
2024-04-25 03:23:41 -0400

    UPDATE: conditional animations

--- src/layouts/ThreeBoxesContainer.jsx
+++ src/layouts/ThreeBoxesContainer.jsx
@@ -1,19 +1,29 @@
 import { useScroll, useTransform, motion } from "framer-motion";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function ThreeBoxesContainer({ leftBox, leftClassName, middleBox, middleClassName, rightBox, rightClassName }) {
     const ref = useRef(null);
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
     const { scrollYProgress } = useScroll({
         target: ref,
         offset: ["end start", "end end"],
     });
 
-    const opacity = useTransform(scrollYProgress, [0, 0.5], [0, 1]);
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    const y1 = useTransform(scrollYProgress, [0, 0.5], [0, -100]);
+
     return (
         <motion.div
-            style={{ opacity: opacity }}
-            ref={ref}
-            className="grid grid-cols-10 overflow-hidden relative md:grid-rows-3">
+            className="grid grid-cols-10 relative md:grid-rows-3">
             <motion.div
                 className={`col-span-10 md:col-start-1 md:col-span-3 md:row-start-1 md:row-span-1 z-30 ${leftClassName}`}
             >
@@ -25,6 +35,8 @@ function ThreeBoxesContainer({ leftBox, leftClassName, middleBox, middleClassNam
                 {rightBox}
             </motion.div>
             <motion.div
+                style={isMobile ? {y: 0}: {y: y1}}
+                ref={ref}
                 className={`col-span-10 md:col-start-2 md:col-span-5 md:row-start-2 md:row-span-1 z-20 ${middleClassName}`}
             >
                 {middleBox}
